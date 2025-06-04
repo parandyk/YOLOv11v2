788131fcd72b006bd5525a97db637779c8c2b784
parandyk <mparandyk@interia.pl>
2025-06-04 13:50:32 +0200

    Update main.sh
    debugging

--- main.sh
+++ main.sh
@@ -1,4 +1,7 @@
+# GPUS=$1
+# cd $2
+# pwd
+# python3 -m torch.distributed.launch --nproc_per_node=$GPUS main.py ${@:3}
+
 GPUS=$1
-cd $2
-pwd
-python3 -m torch.distributed.launch --nproc_per_node=$GPUS main.py ${@:3}
+python3 -m torch.distributed.launch --nproc_per_node=$GPUS main.py ${@:2}
