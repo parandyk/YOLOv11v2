78d03fa060841f60a61780995d0c01386d509630
parandyk <mparandyk@interia.pl>
2025-06-06 15:03:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -186,7 +186,7 @@ def train(args, params):
                         if "momentum" in x:
                             x["momentum"] = np.interp(glob_step, xi, [0.8, 0.937])
                 #print(f'to batch: {batch}')
-                images = images.cuda().float() / 255
+                images = (images.cuda().float()) / 255
                 with torch.amp.autocast("cuda"):
                     pred = model(images)
                     loss, loss_items = criterion(pred, batch)
@@ -281,7 +281,7 @@ def validate(args, params, model=None):
     for batch in tqdm.tqdm(loader, desc=('%10s' * 5) % (
     '', 'precision', 'recall', 'mAP50', 'mAP')):
         images, targets = batch
-        images = images.cuda().float()) / 255
+        images = (images.cuda().float()) / 255
         for k in ["idx", "cls", "box"]:
             batch[k] = batch[k].cuda()
 
